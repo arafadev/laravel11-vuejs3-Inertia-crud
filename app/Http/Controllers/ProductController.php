f4a00039f0c5513ffa98bd4cdab8b826c8051746
Arafa Dev <arafa.dev@gmail.com>
2024-08-24 15:30:48 +0300

    create data & validate data by inertiajs

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Inertia::render('Frontend/Product/Index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('Frontend/Product/Create');
+
+    }
+
+    /**
+     * Store a newly created resource in storage. 
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|integer',
+        ]);
+
+        Product::create([
+            'name' => $request->name,
+            'price' => $request->price,
+        ]);
+        return redirect()->to('/products')->with('message', 'Product Created Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
